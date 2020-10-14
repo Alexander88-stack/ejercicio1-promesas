fc1f0b03a7369d826099b8a4f5f850e223468f5b
Commit <>
2020-10-14 11:53:31 +0200

    siguiendo pasos dentro del ejercicio

--- promesas_practica/promesas.js
+++ promesas_practica/promesas.js
@@ -2,7 +2,7 @@ console.log('Hola mundo')
 
 const goodbyePromise = () => {
     return new Promise((resolve,reject) => {
-        if(false) {
+        if(true) {
             setTimeout( () => {
                 resolve('BYE BYE!');
             },1000);
@@ -13,4 +13,5 @@ const goodbyePromise = () => {
 }
 goodbyePromise()
 .then(response => console.log('Respuesta es:',response))
+.then(response => console.log('Vaya lo has liado parda !!!!',response))
 .catch(error => console.log('Has motado el lio',error))
